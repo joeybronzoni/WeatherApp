ce0dc25e67ed6cf4d50514c9403414c627b73e44
josephskrzysowski <josephskrzysowski@gmail.com>
2017-10-15 17:18:46 -0400

    fixing my ajax call. Problem parsing

--- javascript/app.js
+++ javascript/app.js
@@ -4,78 +4,58 @@
 3)make an ajax call to weather api using the coordinates
 4)
 */
-
-
-$(document).ready(function() {
-var key ="&APPID=4e93dcc7568b48181fe68e883aede969"; 
-var url ="api.openhweathermap.org/data/2.5/weather?";
-
-var Geo={};//push geolocation into Geo={}
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success,error);
- }
-else {
-  alert('Geolocation is not supported');
-}
-
-
-function error() {
-  alert("That's weird! We couldn't find you!");
-}
-
-
-function success(position) {
-  Geo.lat = position.coords.latitude;
-  Geo.lng = position.coords.longitude;
-  alert("We found you!" );
-  $("#data").html("latitude: " + Geo.lat + "<br>longitude: " + Geo.lng);
-}
- var Weather = url+ "lat="+Geo.lat+'&lon='+Geo.lng+key;
-console.log(Weather);
-$.ajax({
-  url: Weather,
-  method: "GET",
-  dataType: "json"
-}).done((res) => {
-  console.log(res);
- // let results = res.quote;
-  //let me = $.parseHTML(results)
- // $(".display").html("<div id='newDiv'>" + "<h1>" + results.author + "</h1>" + "<h2>" + results.tags + "</h2>" + "<p>" + results.body + "</p>" + "</div>");
-});
-
-//var Weather = "http://api.wunderground.com/api/”+ key +”/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
-
-//console.log(Weather);
-
-
-
-
-
-
-
- 
+//$(document).ready(function() {
   
+  var key ="&APPID=4e93dcc7568b48181fe68e883aede969"; 
+  var url ="api.openhweathermap.org/data/2.5/weather?";
+  var Geo={};//push geolocation into Geo={}
+  // var Weather = "http://api.wunderground.com/api/”+ key +”/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
+  var WeatherUrl = "api.openweathermap.org/data/2.5/weather?"+"lat="+Geo.lat+'&lon='+Geo.lng+key;
 
-
-
-
-
-
-
-
-
-
-
-//my testing for coordinates
-function getCoordinates(arg) {
-  // Only change code below this line.
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position)=>{
-      $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-      var Weather = url+ "lat="+position.coords.latitude+'&lon='+position.coords.longitude+key;
-      console.log(Weather);
-    });
-  };
-}
-//getCoordinates();
-});
+    navigator.geolocation.getCurrentPosition(success,error);
+  }
+  else {
+    alert('Geolocation is not supported');
+  }
+  function error() {
+    alert("That's weird! We couldn't find you!");
+  }
+  console.log("Geo",Geo);
+  //----------------------------------------------------------------------------------------
+  function success(position) {
+    Geo.lat = position.coords.latitude;
+    Geo.lng = position.coords.longitude;
+    alert("We found you!" );
+    $("#data").html("latitude: " + Geo.lat + "<br>longitude: " + Geo.lng);
+    WeatherUrl = "api.openweathermap.org/data/2.5/weather?"+"lat="+Geo.lat+'&lon='+Geo.lng+key;
+    console.log(WeatherUrl);
+    hitApi();
+  }
+  function hitApi(res) {
+    console.log(WeatherUrl);
+    $.ajax({
+      url: WeatherUrl,
+      method: "GET",
+      dataType: "jsonp",
+    }).done(function(res, err) {
+      console.log(res);
+      
+    }).fail((jqXHR, textStatus, errorThrown) => {
+
+      console.log(jqXHR.status);
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+);
+
+
+    
+  }  
+
+  //----------------------------------------------------------------------------------------
+
+//});
+
+
+
